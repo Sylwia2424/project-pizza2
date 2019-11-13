669fca6010781109565dd580b9538fe9e2e4e814
Sylwia <sylwia_24_24@wp.pl>
2019-11-13 20:01:58 +0100

    Change booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,31 +1,37 @@
-import { select, templates } from './../settings.js';
+import {select, templates} from './../settings.js';
 import AmountWidget from './AmountWidget.js';
 
 
 class Booking{
-  constructor(){
+  constructor(container){
     const thisBooking = this;
-    
-    thisBooking.initWidgets();
+
     thisBooking.render(container);
-    console.log('new Booking', thisBooking);
+    thisBooking.initWidgets();
+    //console.log('new Booking', thisBooking);
 
   }
-  render(){
+  render(container){
     const thisBooking = this;
 
     const generatedHTML = templates.bookingWidget();
-    thisBooking.dom = '';
-    thisBooking.dom.wrapper = thisBooking.dom;
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = container;
     thisBooking.dom.wrapper = generatedHTML;
-    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.quertySelector(select.booking.peopleAmount);
-    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.quertySelector(select.booking.hoursAmount);
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
   }
   initWidgets(){
     const thisBooking = this;
 
-    thisBooking.peopleAmount = new AmountWidget(select.booking.peopleAmount);
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    //thisBooking.dom.peopleAmount.addEventListener('updated', function(){
+
+    //});
     thisBooking.hoursAmount = new AmountWidget(select.booking.hoursAmount);
+    //thisBooking.dom.hoursAmount.addEventListener('updated', function(){
+
+    //});
   }
 }
 export default Booking;
