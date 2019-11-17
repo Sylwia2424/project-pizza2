0d34022bb6a904746a2347ac870d68dd452bf934
Sylwia <sylwia_24_24@wp.pl>
2019-11-17 15:45:27 +0100

    Add range-silder

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -24,14 +24,14 @@ class Booking{
     container.appendChild(thisBooking.dom.wrapper.element);
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
-    //thisBooking.dom.datePicker.wrapper = thisBooking.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
   }
   initWidgets(){
     const thisBooking = this;
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
-    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker.wrapper);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
 
   }
 }
--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,7 +1,7 @@
+/* global flatpickr */
 import utils from '../utils.js';
 import {select, settings} from '../settings.js';
 import BaseWidget from './BaseWidget.js';
-import flatpickr from 'flatpickr';
 
 class DatePicker extends BaseWidget{
   constructor(wrapper){
@@ -10,31 +10,34 @@ class DatePicker extends BaseWidget{
     const thisWidget = this;
 
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
-
+    thisWidget.initPlugin();
+    thisWidget.parseValue();
+    thisWidget.isValid();
+    thisWidget.renderValue();
   }
   initPlugin(){
     const thisWidget = this;
 
     thisWidget.minDate = new Date(thisWidget.value);
-    thisWidget.maxDate >= thisWidget.minDate.querySelector(settings.datePicker.maxDaysInFuture);
+    thisWidget.maxDate >= thisWidget.minDate == settings.datePicker.maxDaysInFuture;
+    thisWidget.addDays = utils.addDays(thisWidget.minDate);
 
     let calendar = flatpickr(thisWidget.dom.input, {
       
-      minDate: thisWidget.minDate,
-      maxDate: thisWidget.maxDate,
+      'minDate': thisWidget.minDate,
+      'maxDate': thisWidget.maxDate,
       'disable': [
         function(date) {
           // return true to disable
-          return (date.getDay() === 0 || date.getDay() === 6);
-
+          return (date.getDay() === 1);
         }
       ],
       'locale': {
         'firstDayOfWeek': 1 // start week on 
 
       },
-      onChange: function(dateStr) {
-        thisWidget.value = dateStr;
+      onChange: function() {
+        thisWidget.value = thisWidget.addDays;
         //...
       },
     });
